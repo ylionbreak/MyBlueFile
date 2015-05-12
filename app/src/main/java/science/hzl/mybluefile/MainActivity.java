3bae31f87b1936010e9335151da027a7492967be
YLion <739437820@qq.com>
2015-05-12 22:05:53 +0800

    last version

--- app/src/main/java/science/hzl/mybluefile/MainActivity.java
+++ app/src/main/java/science/hzl/mybluefile/MainActivity.java
@@ -8,10 +8,14 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,11 +26,10 @@ public class MainActivity extends ActionBarActivity {
 
 	BluetoothManager bluetoothManager=new BluetoothManager();
 	BluetoothAdapter bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
-	TextView progress;
+	static TextView progress;
 	EditText beConnectedText;
 	EditText fileText;
-	Handler myHandler;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,35 +39,15 @@ public class MainActivity extends ActionBarActivity {
 		Button send = (Button)findViewById(R.id.send);
 		beConnectedText = (EditText)findViewById(R.id.connect_text);
 		fileText = (EditText)findViewById(R.id.fileName);
-		SharedPreferences sharedPreferences = getSharedPreferences("3", ActionBarActivity.MODE_PRIVATE);
-		myHandler = new Handler() {
-			public void handleMessage(Message msg) {
-				if(msg.what==2000000000){
-					Toast toastTell;
-					toastTell=Toast.makeText(App.getContext(), "发送完成", Toast.LENGTH_SHORT);
-					toastTell.setGravity(Gravity.TOP, 0, 600);
-					toastTell.show();
-				}else{
-					progress.setText(String.valueOf(msg.what)+"%");
-					if(msg.what==100){
-						Toast toastTell;
-						toastTell=Toast.makeText(App.getContext(), "传输完成", Toast.LENGTH_SHORT);
-						toastTell.setGravity(Gravity.TOP, 0, 600);
-						toastTell.show();
-					}
-				}
+		SharedPreferences sharedPreferences = getSharedPreferences("file", ActionBarActivity.MODE_PRIVATE);
+		bluetoothManager.startServerSocket(bluetoothAdapter, sharedPreferences);
 
-				super.handleMessage(msg);
-
-			}
-		};
-		bluetoothManager.startServerSocket(bluetoothAdapter, sharedPreferences,myHandler);
 		connect.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				//BluetoothDevice device = bluetoothAdapter.getRemoteDevice(beConnectedText.getText().toString());
+				BluetoothDevice device = bluetoothAdapter.getRemoteDevice(beConnectedText.getText().toString());
 				//BluetoothDevice device = bluetoothAdapter.getRemoteDevice("22:22:CC:06:08:C9");
-				BluetoothDevice device = bluetoothAdapter.getRemoteDevice("10:FA:CE:84:4A:B7");
+				//BluetoothDevice device = bluetoothAdapter.getRemoteDevice("10:FA:CE:84:4A:B7");
 				//BluetoothDevice device = bluetoothAdapter.getRemoteDevice("18:DC:56:D3:26:D1");
 				bluetoothManager.connectDevice(device);
 
@@ -77,8 +60,8 @@ public class MainActivity extends ActionBarActivity {
 					@Override
 					public void run() {
 						try {
-							//bluetoothManager.SendBlueToothFile(0,myHandler,fileText.getText().toString());
-							bluetoothManager.SendBlueToothFile(0,myHandler,"123.jpg");
+							bluetoothManager.SendBlueToothFile(0,fileText.getText().toString());
+							//bluetoothManager.SendBlueToothFile(0,"123.jpg");
 						}catch (Exception e){
 							e.printStackTrace();
 						}
